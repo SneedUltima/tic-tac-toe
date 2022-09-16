8577f6b7f7f81a4e885bbdacd523b9e916dca05b
Dylan <dylan.cobham@yahoo.com.au>
2022-09-16 19:39:48 +0930

    build: add display function which allow user to input X or O on grid display

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const boardContainer = document.querySelector(".game-board")
 
+//  Player factory function
+function player(symbol, score, turn) {
+    return {
+        symbol, score, turn
+    }
+}
+
+const player1 = player("X", 0, true)
+const player2 = player("O", 0, false)
+
 const gameBoard = (() => {
     let board = ["","","","","","","","",""];
     for(i = 0; i < board.length; i++) {
         const div = document.createElement("div")
-        div.addEventListener("click", () => {
-            console.log("hello");
+        div.addEventListener("click", (e) => {
+            displayController(e)
         })
         div.classList.add(`grid`, `grid-${i}`)
         div.textContent = board[i]
@@ -14,20 +24,18 @@ const gameBoard = (() => {
     return board;
 })();
 
-//  Player factory function
-function player(symbol, score, turn) {
-    return {
-        symbol, score, turn
+const displayController = function(e) {
+    const grid = document.querySelector(".grid")
+    if(player1.turn && !player2.turn) {
+        e.target.textContent = player1.symbol
+        player1.turn = false;
+        player2.turn = true;
     }
-}
-
-const player1 = player("x", 0, 0)
-const player2 = player("o", 0, 0)
-
-const displayController =(() => {
-    if(player1.turn === 0 && player2.turn === 0) {
-        console.log("hello");
+    else if(!player1.turn && player2.turn) {
+        e.target.textContent = player2.symbol
+        player1.turn = false;
+        player2.turn = true;
     }
-})();
+};
 
 console.log(player1.symbol);
