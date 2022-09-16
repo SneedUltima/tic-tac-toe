d1a3e489024026295d94fdab28592b2acb86ea5d
Dylan <dylan.cobham@yahoo.com.au>
2022-09-16 22:35:26 +0930

    fix: board array now in object and modify event listener to change board array on click

--- index.js
+++ index.js
@@ -11,30 +11,38 @@ const player1 = player("X", 0, true)
 const player2 = player("O", 0, false)
 
 const gameBoard = (() => {
-    let board = ["","","","","","","","",""];
-    for(i = 0; i < board.length; i++) {
-        const div = document.createElement("div")
-        div.addEventListener("click", (e) => {
-            displayController(e)
+    const board = { 
+        gameBoard: ["","","","","","","","",""]
+    };
+    board.gameBoard.forEach(function(element,index) {
+            const div = document.createElement("div")
+            div.addEventListener("click", (e) => {
+                displayController(e, index)
+            })
+            div.classList.add(`grid`, `grid-${board.gameBoard[index]}`)
+            div.textContent = board.gameBoard[index]
+            boardContainer.appendChild(div)
         })
-        div.classList.add(`grid`, `grid-${i}`)
-        div.textContent = board[i]
-        boardContainer.appendChild(div)
-    }
-    return board;
-})();
+        return {board};
+    })();
 
-const displayController = function(e) {
-    const grid = document.querySelector(".grid")
+const displayController = function(e, index) {
+    board = gameBoard.board.gameBoard
+    // if(board[0],board[1],board[2] === "X"){
+    //     console.log("player 1 win");
+    // }
     if(player1.turn && !player2.turn) {
         e.target.textContent = player1.symbol
+        board[index] = player1.symbol
         player1.turn = false;
         player2.turn = true;
+        console.log(board);
     }
     else if(!player1.turn && player2.turn) {
         e.target.textContent = player2.symbol
-        player1.turn = false;
-        player2.turn = true;
+        board[index] = player2.symbol
+        player1.turn = true;
+        player2.turn = false;
     }
 };
 
