668e841ddf205f14333db3f0c61d06383490f262
Dylan <dylan.cobham@yahoo.com.au>
2022-09-18 18:55:22 +0930

    build: add function which check gameboard array for symbol win conditions to return a winner

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ function player(symbol, score, turn) {
 const player1 = player("X", 0, true)
 const player2 = player("O", 0, false)
 
+const winConditions = [
+    [0,1,2],
+    [3,4,5],
+]
+
 const gameBoard = (() => {
     const board = { 
         gameBoard: ["","","","","","","","",""]
@@ -18,6 +23,8 @@ const gameBoard = (() => {
             const div = document.createElement("div")
             div.addEventListener("click", (e) => {
                 displayController(e, index)
+                checkWin(player1.symbol)
+                checkWin(player2.symbol)
             })
             div.classList.add(`grid`, `grid-${board.gameBoard[index]}`)
             div.textContent = board.gameBoard[index]
@@ -28,15 +35,11 @@ const gameBoard = (() => {
 
 const displayController = function(e, index) {
     board = gameBoard.board.gameBoard
-    // if(board[0],board[1],board[2] === "X"){
-    //     console.log("player 1 win");
-    // }
     if(player1.turn && !player2.turn) {
         e.target.textContent = player1.symbol
         board[index] = player1.symbol
         player1.turn = false;
         player2.turn = true;
-        console.log(board);
     }
     else if(!player1.turn && player2.turn) {
         e.target.textContent = player2.symbol
@@ -46,4 +49,35 @@ const displayController = function(e, index) {
     }
 };
 
-console.log(player1.symbol);
+const checkWin = function(symbol) {
+    board = gameBoard.board.gameBoard
+    for(condition in winConditions){
+        if(board[winConditions[0]] && board[winConditions[1]] && board[winConditions[2]] === symbol){
+            console.log("winner");
+        }
+    }
+    // if(board[0] && board[1] && board[2] === symbol){
+    //     console.log("winner");
+    // }
+    // if(board[3] && board[4] && board[5] === symbol){
+    //     console.log("winner");
+    // }
+    // if(board[6] && board[7] && board[8] === symbol){
+    //     console.log("winner");
+    // }
+    // if(board[0] && board[3] && board[6] === symbol){
+    //     console.log("winner");
+    // }
+    // if(board[1] && board[4] && board[7] === symbol){
+    //     console.log("winner");
+    // }
+    // if(board[2] && board[5] && board[8] === symbol){
+    //     console.log("winner");
+    // }
+    // if(board[0] && board[4] && board[8] === symbol){
+    //     console.log("winner");
+    // }
+    // if(board[2] && board[4] && board[6] === symbol){
+    //     console.log("winner");
+    // }
+}
